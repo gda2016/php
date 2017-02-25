624ab56476f3f077e4bfcbcffc9ba7aca5b87b23
gda <gda10672@gmail.com>
2017-02-25 08:30:17 +0200

    Initial commit

---
+++ w3php/syntax.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?><br/>
+
+<!-- PHP Case Sensitivity -->
+
+
+<?php
+ECHO "<h3>PHP Case Sensitivity</h3>";
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+?>
+
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+
+</body>
+</html>
